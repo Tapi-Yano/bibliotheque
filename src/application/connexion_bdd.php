be0e7b067d9246c2caa40f3d100a65ed00f20f97
clementrichard <clement.richard.info@gmail.com>
2020-04-27 00:11:28 +0200

    nouvelle branche clement

--- src/application/connexion_bdd.php
+++ src/application/connexion_bdd.php
@@ -1,11 +1,11 @@
 <?php
 try{
-	$pdo = new PDO('mysql:host=localhost;dbname=tp6;charset=UTF8', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]); 
-	# sert à faire le lien entre ma base de données et mon fichier  
-	# sert à renseigner et executer la base données
+    $pdo = new PDO('mysql:host=localhost;port=3308;dbname=tp6','root','', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]); 
+    # sert à faire le lien entre ma base de données et mon fichier  
+    # sert à renseigner et executer la base données
 }
 catch (Exception $e)
 {
-	die('Erreur : ' . $e->getMessage($message));
+    die('Erreur : ' . $e->getMessage($message));
 }
