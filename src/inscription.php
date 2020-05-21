aaf21d3059d57c8a8c46c3c6225f3639afb78f78
Calibet Yann <yanncalibet@gmail.com>
2020-05-22 00:04:39 +0200

    création confirmation email à finir

--- src/inscription.php
+++ src/inscription.php
@@ -8,6 +8,32 @@ if(empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['mail']) || e
         background-color: green;
         padding: 3%"><b>Veuillez remplir tout le formulaire !!</b></p>';
 }else{
+    // variables du formulaire
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $mail = $_POST['mail'];
+    // confirmation par mail
+    // Mail
+    $header="MIME-Version: 1.0\r\n";
+    $header.='From:"[VOUS]"<yano.c@hotmail.com>'."\n";
+    $header.='Content-Type:text/html; charset="uft-8"'."\n";
+    $header.='Content-Transfer-Encoding: 8bit';
+    $objet = 'Confirmation de votre inscription' ;
+    $message = '
+    <html>
+    <head>
+        <title>Vous vous êtes inscrit sur notre site ...</title>
+    </head>
+    <body>
+        <p>Bonjour Mr/Mmme '. $nom .' '. $prenom .'</p>
+        <p>blablablabla</p>
+    </body>
+    </html>';
+                        
+    //Envoi du mail
+    mail($mail, $objet, $message, $header);
+    var_dump(mail());
+
     $query = $pdo->prepare('
     INSERT INTO membres (nom, prenom, email, mot_de_passe, admin)
     VALUES (?,?,?,?,0)
