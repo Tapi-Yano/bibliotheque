225ba1709fc0c89ec0353cb1d40268efd49be198
Calibet Yann <yanncalibet@gmail.com>
2020-10-03 15:59:03 +0200

    update index undefind page anneeLivres

--- src/anneeLivres.php
+++ src/anneeLivres.php
@@ -11,18 +11,24 @@ $query=$pdo->prepare('
     ORDER BY titre;
 
 ');
-$query->execute(array($_POST['annee']));/* récuperation via le formulaire de l'annee rentrer */ 
+if(isset($_POST['annee'])){
+    $annee = $_POST['annee'];
+}
+$query->execute(array($annee));/* récuperation via le formulaire de l'annee rentrer */ 
 $anneeLivres = $query->fetchAll();
 $message_erreur = "<p style='text-align: center;background-color: red;color: white;padding: 2%;'>Désolé aucun livre de la bibliothèque n'a été publié cette année là</p>";
 $message_valider = "<p style='text-align: center;background-color: green;color: white;padding: 2%;'>Livre trouvé</p>";
 // var_dump($anneeLivres[0]['annee']);
-    foreach($anneeLivres as $liste){
-        if($_POST['annee'] != $liste['annee']){
-            echo $message_erreur;
-            // header('Location: anneeLivres.php');   
-        }else{
-            echo $message_valider;
-        }
+foreach($anneeLivres as $liste){
+    if($_POST['annee'] != $liste['annee']){
+        echo $message_erreur;
+        // header('Location: anneeLivres.php');   
+    }else{
+        echo $message_valider;
     }
-    $template = 'anneeLivres';
-    include 'layout.phtml';
+}
+
+
+
+$template = 'anneeLivres';
+include 'layout.phtml';
